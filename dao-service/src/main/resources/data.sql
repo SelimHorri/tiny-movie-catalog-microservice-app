48084a455c28fc16c9c788dd49c59f06bfe9554b
SelimHorri <horri.slim@gmail.com>
2021-07-31 23:53:38 +0100

    add data file

---
+++ dao-service/src/main/resources/data.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO directors (fname, lname, hiredate) VALUES
+('christopher', 'nolan', '1990-01-01'),
+('ridley', 'scott', '1970-01-01'),
+('quentin', 'tarantino', '1990-01-01');
+
+
+
+INSERT INTO movies (title, imdb_rating, director_id) VALUES
+('Interstellar', 8.6, 1),
+('TENET', 7.4, 1),
+('The Dark Knight', 9, 1),
+('Inglorious Basterds', 8.3, 3),
+('Gladiator', 8.5, 2),
+('Inception', 8.8, 1),
+('Dunkirk', 7.8, 1);
