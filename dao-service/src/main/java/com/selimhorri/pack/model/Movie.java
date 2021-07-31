fe1ecf60778e363abb9afd80ca9d1f8fa31c8e66
SelimHorri <horri.slim@gmail.com>
2021-07-31 17:40:39 +0100

    create movie entity

--- dao-service/src/main/java/com/selimhorri/pack/model/Movie.java
+++ dao-service/src/main/java/com/selimhorri/pack/model/Movie.java
@@ -1,20 +1,16 @@
 package com.selimhorri.pack.model;
 
 import java.io.Serializable;
-import java.time.LocalDate;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,33 +18,30 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "directors")
+@Table(name = "movies")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Builder
-public final class Director implements Serializable {
+public final class Movie implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "director_id")
-	private Integer directorId;
+	@Column(name = "movie_id")
+	private Integer movieId;
 	
-	private String fname;
+	private String title;
 	
-	private String lname;
+	@Column(name = "imdb_rating")
+	private Double imdbRating;
 	
-	@DateTimeFormat(pattern = "dd-MM-yyyy")
-	private LocalDate hiredate;
-	
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "director", cascade = CascadeType.ALL)
-	private Set<Movie> movies;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "director_id")
+	private Director director;
 	
 }
-
-
 
 
 
