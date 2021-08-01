f32bbbdce9bc45464b9a1a90ecd87fa7086f3f6b
SelimHorri <horri.slim@gmail.com>
2021-08-01 17:25:23 +0100

    add eureka client dependency for services

--- movie-service/src/main/java/com/selimhorri/pack/config/RestTemplateConfig.java
+++ movie-service/src/main/java/com/selimhorri/pack/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.selimhorri.pack.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
