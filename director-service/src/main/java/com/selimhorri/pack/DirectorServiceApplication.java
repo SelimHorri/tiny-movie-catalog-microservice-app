72e112e95dc75ecce4fb2fb2348508229390448e
SelimHorri <horri.slim@gmail.com>
2021-08-08 18:16:24 +0100

    add api-gateway and refactor some configs

--- director-service/src/main/java/com/selimhorri/pack/DirectorServiceApplication.java
+++ director-service/src/main/java/com/selimhorri/pack/DirectorServiceApplication.java
@@ -2,8 +2,10 @@ package com.selimhorri.pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DirectorServiceApplication {
 	
 	public static void main(String[] args) {
